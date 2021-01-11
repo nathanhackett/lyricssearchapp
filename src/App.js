76be1513f11d81d9b3d0cfd0087c1ac705f99d86
u190087 <nathan.hackett.2019@mumail.ie>
2021-01-11 17:53:07 +0000

    Co-authored-by: NATHAN BRUDELL <NATHAN.BRUDELL.2019@MUMAIL.IE>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";//import this in terminal
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+          <Route exact path="/" Component={Index} />
+          </Switch>
+        </div>
+      </React.Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
