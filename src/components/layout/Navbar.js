ca377284ff6dfc544c2929d93f06aca2c04db90c
u190087 <nathan.hackett.2019@mumail.ie>
2021-01-13 13:54:10 +0000

    comments

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         // https://www.w3schools.com/bootstrap4/bootstrap_colors.asp
         <nav className="navbar navbar-light bg-light">
-            {/* https://getbootstrap.com/docs/4.0/components/navbar/ */}
+            {/* https://getbootstrap.com/docs/5.0/components/navbar/ */}
             <span className="navbar-brand mb-0 h1 mx-auto">Song Lyrics Search</span>
         </nav>
     )
